10828bd2eb2c507f503606f7ab9d747ed3002abb
mrsfzcn <mrsfzcn@gmail.com>
2023-09-28 09:11:07 +0300

    commit

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -71,6 +71,8 @@ function App() {
           </Routes>
         </section>
       </main>
+
+      <footer></footer>
     </>
   );
 }
--- react-app/src/pages/home/home.jsx
+++ react-app/src/pages/home/home.jsx
@@ -1,7 +1,72 @@
-export function Home(){
-    return (
-        <section >
-            <img className="img-home" src="./public/images/1.jpg" alt="" />
-        </section>
-    )
+export function Home() {
+  return (
+    <div className="container-home">
+      <section>
+        <h1>İnsan Kaynakları Yönetim Platformuna Hoşgeldiniz!</h1>
+        <h5>
+          Bu platform, şirketlerin ihtiyaç duyduğu bilgi ve hizmetlere erişim
+          sağlamak amacıyla oluşturulmuştur.
+        </h5>
+      </section>
+      <section>
+        <h1>Site Bilgileri</h1>
+        <ul>
+          <li>
+            Platformumuz, şirketlerin iş süreçlerini optimize etmek ve
+            verimliliği artırmak için tasarlanmıştır.
+          </li>
+          <li>
+            Kullanıcı dostu arayüzü ve kolay kullanılabilirlik özellikleriyle,
+            şirketlerin ihtiyaç duyduğu bilgilere hızlı erişim sağlar.
+          </li>
+          <li>
+            Güvenli bir ortamda verilerinizi saklamak ve gizliliklerini korumak
+            için modern güvenlik önlemleri kullanırız.
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h1>Platformun Amacı</h1>
+        <ul>
+          <li>
+            Amacımız, şirketlerin rekabet gücünü artırmak ve büyümelerine
+            katkıda bulunmaktır.
+          </li>
+          <li>
+            İş süreçlerini optimize etmek, verimliliği artırmak ve karar verme
+            süreçlerini desteklemek için çeşitli hizmetler sunmaktayız.
+          </li>
+          <li>
+            Şirketlerin ihtiyaçlarına uygun çözümler sunarak, onlara rekabet
+            avantajı sağlamayı hedefliyoruz.
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h1>Ortaklarımız</h1>
+        <ul>
+          <li>
+            İşletmelerin her ölçekteki ihtiyaçlarına cevap verebilmek için geniş
+            bir müşteri tabanına hizmet sunuyoruz.
+          </li>
+          <li>
+            Küçük ve orta ölçekli işletmelerden büyük kuruluşlara kadar birçok
+            farklı sektörde faaliyet gösteren şirketlere yardımcı olmaktayız.
+          </li>
+          <li>
+            Müşteri memnuniyetini ön planda tutarak, en iyi hizmeti sunmak için
+            çalışıyoruz.
+          </li>
+          <li>
+            Bu platform, şirketlerin ihtiyaç duyduğu bilgi ve hizmetlere erişimi
+            kolaylaştırmayı hedeflemektedir.
+          </li>
+          <li> Siz de bu platforma katılarak iş
+            süreçlerinizi optimize edebilir, verimliliğinizi artırabilir ve
+            rekabet avantajı elde edebilirsiniz.
+            </li>
+        </ul>
+      </section>
+    </div>
+  );
 }
