0f331fe16405ce0156fab2e25fc8ff2f7fc0fbf8
efeaks88 <efe.aks@hotmail.com>
2023-10-02 22:23:35 +0300

    dev-HomePage

--- react-app/src/pages/home/home.jsx
+++ react-app/src/pages/home/home.jsx
@@ -1,4 +1,9 @@
+import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
+
 export function Home() {
+
 
   return (
     <div className="container-home">
@@ -65,9 +70,164 @@ export function Home() {
           <li> Siz de bu platforma katılarak iş
             süreçlerinizi optimize edebilir, verimliliğinizi artırabilir ve
             rekabet avantajı elde edebilirsiniz.
-            </li>
+          </li>
         </ul>
+        <h1>İş Ortaklarımız</h1>
+        <div >
+          <div className="findCompany">
+            <FindWithName />
+          </div>
+          <hr></hr>
+          <div className="companylist">
+            <FindAllCompany />
+          </div>
+
+
+        </div>
       </section>
+
+    </div>
+
+  );
+}
+
+function GetAllCompany() {
+  const url = "http://localhost:9091/api/v1/company/find-all"
+  const options = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  return fetch(url, options)
+    .then((resp) => {
+      if (!resp.ok) {
+        return resp.json().then((error) => {
+          throw new Error(error.message); // API'den dönen hata mesajını yakala ve fırlat
+        });
+      }
+      return resp.json();
+    })
+    .then((data) => {
+      console.log("Veri Tipi:", typeof data);
+      console.log(data);
+      return data;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
+}
+
+
+function GetCompanyWith(companyName) {
+  const url = `http://localhost:9091/api/v1/company/findbycompanyname?companyName=${companyName}`;
+
+  const options = {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  };
+
+  return fetch(url, options)
+    .then((resp) => {
+      if (!resp.ok) {
+        return resp.json().then((error) => {
+          throw new Error(error.message); // API'den dönen hata mesajını yakala ve fırlat
+        });
+      }
+      return resp.json();
+    })
+    .then((data) => {
+      console.log("Veri Tipi:", typeof data);
+      console.log(data);
+      return data;
+    })
+    .catch((err) => {
+      console.error(err.message);
+      throw err;
+    });
+}
+
+function FindAllCompany() {
+  // Şirket verilerini ve hata durumunu saklayacak state'leri tanımlayın
+  const [companyData, setCompanyData] = useState([]);
+  const [error, setError] = useState(null);
+
+  // Sayfa yönlendirmesi yapmak için useNavigate'i kullanın
+  const navigateTo = useNavigate();
+
+  // Şirket düğmesine tıklandığında çalışacak fonksiyon
+  const handleClick = (value) => {
+    // Tıklanan şirketin ayrıntılarına yönlendirme yapın
+    navigateTo(`/companypage?value=${value}`);
+  };
+
+  useEffect(() => {
+    // Şirket verilerini almak için API çağrısı yapın
+    GetAllCompany()
+      .then((data) => {
+        // Verileri state'e kaydedin
+        setCompanyData(data);
+      })
+      .catch((err) => {
+        // Hata durumunda hatayı state'e kaydedin
+        setError(err.message);
+        console.log("Hata:", err.message);
+      });
+  }, []);
+
+  if (error) {
+    // Hata durumunda hata mesajını gösterin
+    return <div className="hata">{error}</div>;
+  }
+
+  return (
+    <div className="btnCompanyContainer">
+      <NavLink to="/findAllCompanyPreviewInformation">
+        {companyData.map((obj) => (
+          <button className="btnCompanyName" key={obj.id} onClick={() => handleClick(obj.id)}>
+            {obj.companyName}
+          </button>
+        ))}
+      </NavLink>
     </div>
   );
 }
+
+
+function FindWithName() {
+  const [companyName, setCompanyName] = useState("");
+  const [databaseData, setDatabaseData] = useState([]);
+
+  const handleQueryClick = () => {
+    if (companyName) {
+      GetCompanyWith(companyName)
+        .then((data) => {
+          setDatabaseData(data);
+        })
+        .catch((err) => console.log("Hata:", err.message));
+    }
+  };
+  return (
+    <div className="findCompany">
+      <input className="findCompanyInput"
+        placeholder="Şirket Bul..."
+        value={companyName}
+        onChange={(e) => setCompanyName(e.target.value)}
+      />
+      <button className="findCompanybtn1"></button>
+      <button className="findCompanybtn2" onClick={() => handleQueryClick(companyName)}>ARA</button>
+      {databaseData.companyName && (
+        <div className="databaseCompany">
+          <h1>{databaseData.companyName}</h1>
+          <hr></hr>
+          <p>{databaseData.logo}</p>
+          <p>{databaseData.companyDistrict}</p>
+        </div>
+      )}
+
+
+    </div>
+  );
+}
+
+
